1ce96fbc7968e805b2bc00303d6c00bea93776cd
Vince Loh <5501845+vinzloh@users.noreply.github.com>
2024-03-24 16:30:56 +0800

    chore: update to `vite`, `pnpm`

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import DraggableTable from './DraggableTable';
-
-render(<DraggableTable />, document.getElementById('root'));
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import DraggableTable from "./DraggableTable";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<DraggableTable />);
